431b3530c9b77eb7f869c6fafe4d01706dda802a
Ben Ramsey <ben@benramsey.com>
2023-09-21 22:20:35 -0500

    update webfinger with acct:ben@ramsey.dev information

--- src/Controller/WebFingerController.php
+++ src/Controller/WebFingerController.php
@@ -5,14 +5,111 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WebFingerController extends AbstractController
+/**
+ * RFC 7033: WebFinger, https://datatracker.ietf.org/doc/html/rfc7033
+ */
+final class WebFingerController extends AbstractController
 {
+    private const CONTENT_TYPE = 'application/jrd+json';
+
+    private const BEN_RAMSEY_DEV = [
+        'aliases' => [],
+        'links' => [
+            [
+                'rel' => 'me',
+                'href' => 'https://ben.ramsey.dev',
+                'type' => 'text/html',
+            ],
+            [
+                'rel' => 'http://webfinger.net/rel/avatar',
+                'href' => 'https://www.gravatar.com/avatar/a0fa77843de8a4a2265bb939180a384b.jpg?s=2000',
+                'type' => 'image/png',
+            ],
+            [
+                'rel' => 'http://webfinger.net/rel/profile-page',
+                'href' => 'https://ben.ramsey.dev',
+                'type' => 'text/html',
+            ],
+            [
+                'rel' => 'self',
+                'href' => 'https://phpc.social/users/ramsey',
+                'type' => 'application/activity+json',
+            ],
+        ],
+        'properties' => [
+            'https://schema.org/name' => 'Ben Ramsey',
+            'https://schema.org/email' => 'ben@ramsey.dev',
+        ],
+        'subject' => 'acct:ben@ramsey.dev',
+    ];
+
+    private const RESOURCES = [
+        'acct:ben@ramsey.dev' => self::BEN_RAMSEY_DEV,
+        'acct:ben@benramsey.com' => self::BEN_RAMSEY_DEV,
+    ];
+
     #[Route('/.well-known/webfinger')]
-    public function handle(): Response
+    public function handle(Request $request): Response
     {
-        return new Response('{}', 200, ['content-type' => 'application/json']);
+        $headers = ['content-type' => self::CONTENT_TYPE];
+
+        $resource = $request->query->getString('resource');
+
+        if (trim($resource) === '') {
+            return new Response('{}', Response::HTTP_BAD_REQUEST, $headers);
+        }
+
+        $requestedRelations = $this->parseRelations($request);
+
+        if (array_key_exists($resource, self::RESOURCES)) {
+            $data = self::RESOURCES[$resource];
+
+            $links = [];
+            foreach ($data['links'] ?? [] as $link) {
+                if (count($requestedRelations) > 0 && !in_array($link['rel'], $requestedRelations)) {
+                    // Skip this link if the relation wasn't requested.
+                    continue;
+                }
+
+                // If the href begins with a "/", turn it into an absolute URL.
+                if (isset($link['href']) && str_starts_with($link['href'], '/')) {
+                    $link['href'] = $request->getUriForPath($link['href']);
+                }
+
+                $links[] = $link;
+            }
+
+            $data['links'] = $links;
+
+            return new JsonResponse(data: $data, headers: $headers);
+        }
+
+        return new Response('{}', Response::HTTP_NOT_FOUND, $headers);
+    }
+
+    private function parseRelations(Request $request): array
+    {
+        $parts = array_map(
+            function (string $value): array {
+                [$k, $v] = explode('=', $value, 2);
+
+                return ['key' => urldecode($k), 'value' => urldecode($v)];
+            },
+            explode('&', $request->server->get('QUERY_STRING')),
+        );
+
+        $relations = [];
+        foreach ($parts as $part) {
+            if (strtolower($part['key']) === 'rel') {
+                $relations[] = $part['value'];
+            }
+        }
+
+        return $relations;
     }
 }
