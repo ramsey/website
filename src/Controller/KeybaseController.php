a16d5e84a69be423b70ce4474bbaf023042fdb5b
Ben Ramsey <ben@benramsey.com>
2023-09-28 16:14:36 -0500

    add a KeybaseController and a keybase.txt well-known endpoint

---
+++ src/Controller/KeybaseController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use function md5;
+
+class KeybaseController extends AbstractController
+{
+    #[Route('/.well-known/keybase.txt')]
+    public function handle(Request $request): Response
+    {
+        $response = new Response();
+        $response->headers->add([
+            'content-type' => 'text/plain',
+        ]);
+
+        $response = match (strtolower($request->getHost())) {
+            'benramsey.com', 'www.benramsey.com' => $this->render(
+                view: 'keybase/benramsey-com.txt.twig',
+                response: $response,
+            ),
+            default => throw $this->createNotFoundException(),
+        };
+
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
+    }
+}
