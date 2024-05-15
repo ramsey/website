99c6544ebc2231abf7a97d774cd7b021c7966b7d
Ben Ramsey <ben@ramsey.dev>
2024-05-15 01:06:35 -0500

    feat: verify the domain/site on Keybase

---
+++ src/Controller/KeybaseController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Util\CacheTtl;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+use function md5;
+use function strtolower;
+
+#[AsController]
+#[Route('/.well-known/keybase.txt')]
+#[Cache(maxage: CacheTtl::Week->value, public: true, staleWhileRevalidate: CacheTtl::Day->value)]
+final readonly class KeybaseController
+{
+    public function __construct(private Environment $twig)
+    {
+    }
+
+    public function handle(Request $request): Response
+    {
+        $content = match (strtolower($request->getHost())) {
+            'ben.ramsey.dev' => $this->twig->render('keybase/ben-ramsey-dev.txt.twig'),
+            'benramsey.com' => $this->twig->render('keybase/benramsey-com.txt.twig'),
+            default => throw new NotFoundHttpException(),
+        };
+
+        $response = new Response($content);
+        $response->headers->add(['content-type' => 'text/plain']);
+        $response->setEtag(md5((string) $response->getContent()));
+        $response->isNotModified($request);
+
+        return $response;
+    }
+}
