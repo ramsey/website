6392e29521725e21993c80ef8ec7da39c21df124
Ben Ramsey <ben@ramsey.dev>
2024-07-12 23:29:21 -0500

    feat: send application logs to Better Stack

---
+++ src/EventListener/RequestLogListener.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of ramsey/website
+ *
+ * Copyright (c) Ben Ramsey <ben@ramsey.dev>
+ *
+ * ramsey/website is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * ramsey/website is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ramsey/website. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Psr\Clock\ClockInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+
+use function number_format;
+
+#[AsEventListener(event: 'kernel.terminate', priority: -1)]
+final readonly class RequestLogListener
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        #[Target('monotonicClock')] private ClockInterface $clock,
+    ) {
+    }
+
+    public function __invoke(TerminateEvent $event): void
+    {
+        /** @var float | string | null $execTime */
+        $execTime = $event->getRequest()->server->get('REQUEST_TIME_FLOAT');
+
+        if ($execTime !== null) {
+            $clockTime = (float) $this->clock->now()->format('U.u');
+            $execTime = number_format($clockTime - (float) $execTime, 6, '.', '');
+        }
+
+        $this->logger->info('Request for {url}', [
+            'url' => $event->getRequest()->getUri(),
+            'request' => [
+                'method' => $event->getRequest()->getMethod(),
+                'headers' => $event->getRequest()->headers->all(),
+            ],
+            'response' => [
+                'code' => $event->getResponse()->getStatusCode(),
+                'headers' => $event->getResponse()->headers->all(),
+            ],
+            'exec_time' => $execTime,
+        ]);
+    }
+}
---
+++ tests/EventListener/RequestLogListenerTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\EventListener;
+
+use App\EventListener\RequestLogListener;
+use DateTimeImmutable;
+use Faker\Factory;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Monolog\Handler\TestHandler;
+use Monolog\LogRecord;
+use Monolog\Logger;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Clock\Test\ClockSensitiveTrait;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+use function json_encode;
+
+#[TestDox('RequestLogListener')]
+class RequestLogListenerTest extends TestCase
+{
+    use ClockSensitiveTrait;
+    use MockeryPHPUnitIntegration;
+
+    #[TestDox('logs request and response info for each request')]
+    public function testLogsRequestResponseData(): void
+    {
+        $faker = Factory::create();
+        $userAgent = $faker->userAgent();
+
+        $requestTime = new DateTimeImmutable('@1720843500.863559');
+        $clock = static::mockTime(new DateTimeImmutable('@1720843501.386580'));
+
+        $testHandler = new TestHandler();
+        $logger = new Logger('test', [$testHandler]);
+
+        $request = Request::create(
+            uri: 'https://foo.example.net/path/to/content',
+            method: 'PUT',
+            cookies: [
+                'testCookie' => 'this cookie should not appear in the logs',
+            ],
+            server: [
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_HOST' => 'foo.example.net',
+                'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'HTTP_ACCEPT_CHARSET' => 'utf-8',
+                'HTTP_ACCEPT_ENCODING' => 'gzip, deflate, br',
+                'HTTP_ACCEPT_LANGUAGE' => 'es-MX',
+                'HTTP_USER_AGENT' => $userAgent,
+                'REQUEST_TIME_FLOAT' => $requestTime->format('U.u'),
+            ],
+            content: (string) json_encode(['foo' => 'bar']),
+        );
+
+        $response = new Response(
+            content: 'Body of response.',
+            status: 201,
+            headers: [
+                'cache-control' => 'no-cache, private',
+                'content-type' => 'text/plain; charset=utf-8',
+                'link' => '<https://example.com/path/to/alt/content>; rel=alternate',
+                'date' => 'Sat, 13 Jul 2024 04:17:53 GMT',
+            ],
+        );
+
+        $kernel = Mockery::mock(HttpKernelInterface::class);
+
+        $listener = new RequestLogListener($logger, $clock);
+        $event = new TerminateEvent($kernel, $request, $response);
+        $listener($event);
+
+        $this->assertTrue($testHandler->hasInfoThatPasses(function (LogRecord $record) use ($userAgent): bool {
+            $this->assertSame('Request for {url}', $record->message);
+            $this->assertSame(
+                [
+                    'url' => 'https://foo.example.net/path/to/content',
+                    'request' => [
+                        'method' => 'PUT',
+                        'headers' => [
+                            'host' => ['foo.example.net'],
+                            'user-agent' => [$userAgent],
+                            'accept' => ['text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'],
+                            'accept-language' => ['es-MX'],
+                            'accept-charset' => ['utf-8'],
+                            'content-type' => ['application/json'],
+                            'accept-encoding' => ['gzip, deflate, br'],
+                        ],
+                    ],
+                    'response' => [
+                        'code' => 201,
+                        'headers' => [
+                            'cache-control' => ['no-cache, private'],
+                            'content-type' => ['text/plain; charset=utf-8'],
+                            'link' => ['<https://example.com/path/to/alt/content>; rel=alternate'],
+                            'date' => ['Sat, 13 Jul 2024 04:17:53 GMT'],
+                        ],
+                    ],
+                    'exec_time' => '0.523021',
+                ],
+                $record->context,
+            );
+
+            return true;
+        }));
+    }
+}
